d4d475650ab1f13207837d04654cbb7392ade957
sangnvus <sangnvus@yahoo.com>
2015-03-29 11:23:53 +0700

    Initial commit from local git

---
+++ TNiOSRider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TNiOSRider
+//
+//  Created by admin on 3/29/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TNiOSRider/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  TNiOSRider
+//
+//  Created by admin on 3/29/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
